d223446d2eecd9f0496d2c41987919e064bd02d3
Filip Sufitchi <fsufitch@users.noreply.github.com>
2024-07-30 21:31:06 +0000

    Use absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@app': '/src',
+    },
+    // extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.css'],
+  },
   plugins: [vue()],
 })
